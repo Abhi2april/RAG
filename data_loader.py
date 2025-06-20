ed7c8dc14bdae4e7fabdeb7d0380d709be87fe35
Abhi2april <abksingh2004@gmail.com>
2025-06-20 23:44:03 +0530

    Feat: such that the rag identifies user query  wants to look at order or product dataset

--- data_loader.py
+++ data_loader.py
@@ -1,11 +1,24 @@
 import requests
 import pandas as pd
-from config import NGROK_URL
+#from config import NGROK_URL
 
 def load_data():
-#    headers = {"ngrok-skip-browser-warning": "true"}
-#    response = requests.get(NGROK_URL, headers=headers)
-    df=pd.read_csv('C:/Users/ASUS/Desktop/RAG/Order_Data_Dataset.csv')
-#    df = pd.DataFrame(response.json())
-#    df=df[:1000]
-    return df
+    # loading both the provided datasets in form of csv first for testing purposes, 
+    # so later it can be used via ngrok's api linkage.
+    order_df = pd.read_csv(r'C:\Users\ASUS\Desktop\A-FAST-ECOMMERCE-RAG-CHATBOT-FOR-CUSTOMERS\Order_Data_Dataset.csv')
+    product_df = pd.read_csv(r'C:\Users\ASUS\Desktop\A-FAST-ECOMMERCE-RAG-CHATBOT-FOR-CUSTOMERS\Product_Information_Dataset.csv')
+    return order_df, product_df
+
+#this will tell weather the query is about product dataset or order dataset 
+
+def get_dataset_type(query: str) -> str:
+    
+    product_keywords = ['product', 'item', 'price', 'inventory', 'stock', 'description', 'specification']
+    order_keywords = ['order', 'purchase', 'customer', 'date', 'payment', 'shipping']
+    
+    query_lower = query.lower()
+    
+    product_matches = sum(1 for keyword in product_keywords if keyword in query_lower)
+    order_matches = sum(1 for keyword in order_keywords if keyword in query_lower)
+    
+    return 'product' if product_matches>order_matches else 'order'
