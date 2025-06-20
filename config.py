5dcbeaecd3efafa003948270d0f89657e680be7f
Abhi2april <abksingh2004@gmail.com>
2025-06-20 23:49:41 +0530

    Perf: Changed the embedding model from a Transformer based model to a Static embedding model which is 5x faster

--- config.py
+++ config.py
@@ -1,11 +1,13 @@
 import os
 import requests
-# Load API Key
-GROQ_API_KEY = os.environ.get("GROQ_API_KEY")
+import getpass
+if not os.environ.get("GROQ_API_KEY"):
+  os.environ["GROQ_API_KEY"] = getpass.getpass("Enter API key for groq: ")
 
 # URLs
-NGROK_URL = "https://7ed5-103-47-74-66.ngrok-free.app"
+# NGROK_URL = "https://7ed5-103-47-74-66.ngrok-free.app"
 
 # Model
-EMBEDDING_MODEL_NAME = "sentence-transformers/all-mpnet-base-v2"
+#EMBEDDING_MODEL_NAME = "thenlper/gte-small"
+EMBEDDING_MODEL_NAME = "sentence-transformers/static-retrieval-mrl-en-v1"
 CHROMA_PERSIST_DIR = "chroma_db"
