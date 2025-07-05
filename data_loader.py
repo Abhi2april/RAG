6ee7b2dd6bdba5596cb38d73212dae6cb49871d8
Abhi2april <abksingh2004@gmail.com>
2025-07-05 15:07:34 +0530

    refractor: dataset location

--- data_loader.py
+++ data_loader.py
@@ -5,8 +5,10 @@ import pandas as pd
 def load_data():
     # loading both the provided datasets in form of csv first for testing purposes, 
     # so later it can be used via ngrok's api linkage.
-    order_df = pd.read_csv(r'C:\Users\ASUS\Desktop\A-FAST-ECOMMERCE-RAG-CHATBOT-FOR-CUSTOMERS\Order_Data_Dataset.csv')
-    product_df = pd.read_csv(r'C:\Users\ASUS\Desktop\A-FAST-ECOMMERCE-RAG-CHATBOT-FOR-CUSTOMERS\Product_Information_Dataset.csv')
+#   headers = {"ngrok-skip-browser-warning": "true"}
+#   response = requests.get(NGROK_URL, headers=headers)
+    order_df = pd.read_csv(r'C:\Users\ASUS\Desktop\A-FAST-ECOMMERCE-RAG-CHATBOT-FOR-CUSTOMERS\data\Order_Data_Dataset.csv')
+    product_df = pd.read_csv(r'C:\Users\ASUS\Desktop\A-FAST-ECOMMERCE-RAG-CHATBOT-FOR-CUSTOMERS\data\Product_Information_Dataset.csv')
     return order_df, product_df
 
 #this will tell weather the query is about product dataset or order dataset 
